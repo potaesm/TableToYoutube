d9c13425d0c4bba49e2575b51347fc17c2468541
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2020-06-12 21:54:00 +0700

    Edit

--- index.js
+++ index.js
@@ -2,53 +2,54 @@ const request = require('request-promise');
 const DOMParser = require('xmldom').DOMParser;
 const fs = require('fs-extra')
 
+const sourceLink = 'https://en.wikipedia.org/wiki/List_of_Mystery_Hunters_episodes';
+
 const youtube = 'https://www.youtube.com';
 const youtubeSearch = 'https://www.youtube.com/results?search_query=';
-const title = 'The Most Extreme - ';
-const headOffSet = 2;
-const tailOffset = -5;
+const title = 'Mystery Hunters - ';
+const headOffSet = 0;
+const tailOffset = 0;
 
-const fileName = 'TheMostExtreme.txt';
+const fileName = 'MysteryHunters.txt';
 
 async function main() {
-    await request('https://en.wikipedia.org/wiki/The_Most_Extreme#:~:text=The%20Most%20Extreme%20is%20a,aired%20on%20July%207%2C%202002.')
+    await request(sourceLink)
         .then(async function (htmlString) {
             const document = new DOMParser().parseFromString(htmlString, 'text/html');
-            let length = document.getElementsByTagName("i").length;
+            let length = document.getElementsByTagName("b").length;
             let results = [];
 
             for (let x = 0; x < length; x++) {
-                const normalSearchQuery = encodeURI(title + document.getElementsByTagName("i")[x].textContent);
-                const promise = await request(youtubeSearch + normalSearchQuery)
-                    .then(async function (innerHtmlString) {
-                        const innerDocument = new DOMParser().parseFromString(innerHtmlString, 'text/html');
-                        const search_query_47 = innerDocument.getElementsByTagName("a")[47];
-                        if (search_query_47 === undefined) {
-                            results[x] = youtubeSearch + normalSearchQuery;
-                        } else {
-                            const watchEndpoint = search_query_47.attributes[1].value;
-                            if (watchEndpoint === '_blank' || watchEndpoint.substring(watchEndpoint.length - 9, watchEndpoint.length - 1) === 'spf-link') {
+                const episodeName = document.getElementsByTagName("b")[x].getElementsByTagName("a")[0];
+                if (episodeName !== undefined) {
+                    const normalSearchQuery = encodeURI(title + episodeName.textContent);
+                    const promise = await request(youtubeSearch + normalSearchQuery)
+                        .then(async function (innerHtmlString) {
+                            const innerDocument = new DOMParser().parseFromString(innerHtmlString, 'text/html');
+                            const search_query_47 = innerDocument.getElementsByTagName("a")[47];
+                            if (search_query_47 === undefined) {
                                 results[x] = youtubeSearch + normalSearchQuery;
                             } else {
-                                results[x] = youtubeSearch + normalSearchQuery + ' or ' + youtube + watchEndpoint;
+                                const watchEndpoint = search_query_47.attributes[1].value;
+                                if (watchEndpoint === '_blank' || watchEndpoint.substring(watchEndpoint.length - 9, watchEndpoint.length - 1) === 'spf-link') {
+                                    results[x] = youtubeSearch + normalSearchQuery;
+                                } else {
+                                    results[x] = youtubeSearch + normalSearchQuery + ' or ' + youtube + watchEndpoint;
+                                }
                             }
+                        })
+                        .catch(function (err) {
+                            return console.log(err);
+                        });
+                    Promise.all([promise]).then(() => {
+                        let file = fs.createWriteStream(fileName);
+                        file.on('error', function (err) { console.log(err) });
+                        for (let i = 0 + headOffSet; i < results.length + tailOffset; i++) {
+                            file.write((results[i] === undefined || results[i] === '') ? '\n' : results[i] + '\n');
                         }
-                    })
-                    .catch(function (err) {
-                        return console.log(err);
+                        file.end();
                     });
-                Promise.all([promise]).then(() => {
-                    let file = fs.createWriteStream(fileName);
-                    file.on('error', function (err) { console.log(err) });
-                    for (let i = 0 + headOffSet; i < results.length + tailOffset; i++) {
-                        let line = '\n';
-                        if (results[i] !== undefined || results[i] !== '') {
-                            line = results[i] + '\n';
-                        }
-                        file.write(line);
-                    }
-                    file.end();
-                });
+                }
             }
         })
         .catch(function (err) {
